9563f8dae3898797d35d84345ad48af5f0522ec5
Luca Sorace Stranck <strdjn@gmail.com>
2016-07-04 19:18:25 +0200

    Update Chan.java

--- TestBot/src/main/java/it/TetrisReich/bot/TestBot/Chan.java
+++ TestBot/src/main/java/it/TetrisReich/bot/TestBot/Chan.java
@@ -35,37 +35,4 @@ public class Chan {
 		    }
 		}
 	}
-	/*public static String video;
-	public static String live;
-	public static String chan() throws IOException{
-    	String text = "";
-    	if(App.textEsist==true){           
-    		Random generator = new Random();
-        	toke1();
-    		if(App.inLive==true){
-    			StringTokenizer st = new StringTokenizer(live, ";");
-            	int in = generator.nextInt(3);
-    			for(int i=0;i<=in;i++){
-    				String str;
-    				str = st.nextToken();
-    				if(i==in) text = str;
-    			}
-    		} else {
-    			StringTokenizer st = new StringTokenizer(video, ";");
-            	int in = generator.nextInt(3);
-    			for(int i=0;i<=in;i++){
-    				String str;
-    				str = st.nextToken();
-    				if(i==in) text = str;
-    			}
-    		}
-    	} else if(App.inLive==true)text = "In live ora:"; else text = "Nuovo video:";
-    	return text;
-    }
-	public static String toke1() throws IOException{
-		StringTokenizer st = new StringTokenizer(App.reader("text"), "_");
-        video = st.nextToken();
-        live = st.nextToken();
-        return null;
-	}*/
 }
